b4b0364b320c9ddadc22ad0716f2b34b0e35c387
timzaak <zsy.evan@gmail.com>
2025-05-28 16:54:40 +0800

    add deps

--- lib/pages/account/login_page.dart
+++ lib/pages/account/login_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
